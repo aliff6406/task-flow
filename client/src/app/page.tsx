418e7dcf2d1b9e87ed970544c1f8e8a7e2563c11
Aliff Amir <aliff6406@gmail.com>
2024-10-04 19:10:44 +0100

    remove unclickable folder

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+      hi
+    </main>
+  );
+}
